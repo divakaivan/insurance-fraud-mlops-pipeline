9fa006cdcec9385c1a3427d4dfadd715a81c8b3b
divakaivan <timee98@outlook.com>
2024-07-05 19:27:15 +0900

    [monitoring] load model for monitoring

---
+++ monitoring/load_model.py
@@ -0,0 +1,12 @@
+# Load a model from MLflow and use it for monitoring
+
+import os
+import mlflow
+
+tracking_uri = os.getenv('FRAUD_MODELLING_MLFLOW_TRACKING_URI')
+mlflow.set_tracking_uri(tracking_uri)
+mlflow.set_experiment('Insurance Fraud Detection')
+
+run_id = os.getenv('FRAUD_MODELLING_MLFLOW_RUN_ID')
+model_uri = mlflow.get_run(run_id).info.artifact_uri + '/balanced_rf_model'
+mlflow.artifacts.download_artifacts(model_uri, dst_path='./monitoring/model')
