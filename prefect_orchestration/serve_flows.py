3aa9178f8e0765eec88954482359b72d8aeed418
divakaivan <timee98@outlook.com>
2024-07-06 22:53:01 +0900

    [prettify-orchestration] will use postgres for intermediate datasets

--- prefect_orchestration/serve_flows.py
+++ prefect_orchestration/serve_flows.py
@@ -1,21 +1,11 @@
 from prefect import serve
-from upload_data_with_dummy_to_gcs import upload_data_with_dummy_to_gcs
 from insurance_fraud_model import insurance_fraud_model
 from raw_kaggle_to_gcs import raw_kaggle_to_gcs
-from upload_data_no_dummy_to_gcs import upload_data_no_dummy_to_gcs
 
 if __name__ == "__main__":
     raw_kaggle_to_gcs_deply = raw_kaggle_to_gcs.to_deployment(
         name='Upload Raw Kaggle data to GCS',
         tags=['kaggle', 'raw', 'gcs']
-    )
-    upload_data_no_dummy_to_gcs_deploy = upload_data_no_dummy_to_gcs.to_deployment(
-        name='Upload Data with clean Cat Features to GCS',
-        tags=['data', 'gcs', 'monitoring', 'cat']
-    )
-    upload_data_with_dummy_to_gcs_deploy = upload_data_with_dummy_to_gcs.to_deployment(
-        name='Upload Data with Dummies to GCS',
-        tags=['data', 'gcs']
     )
     model_pipe_flow_deploy = insurance_fraud_model.to_deployment(
         name='Train Insurance Fraud Model',
@@ -24,7 +14,5 @@ if __name__ == "__main__":
 
     serve(
         raw_kaggle_to_gcs_deply,
-        upload_data_no_dummy_to_gcs_deploy,
-        upload_data_with_dummy_to_gcs_deploy, 
         model_pipe_flow_deploy,
     )
