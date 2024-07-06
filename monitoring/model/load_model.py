1618721bb1b6f219b8149a55607ca86490c4530b
divakaivan <timee98@outlook.com>
2024-07-06 22:40:32 +0900

    [prettify-monitoring] move files to their own folders

--- monitoring/model/load_model.py
+++ monitoring/model/load_model.py
@@ -9,4 +9,4 @@ mlflow.set_experiment('Insurance Fraud Detection')
 
 run_id = os.getenv('FRAUD_MODELLING_MLFLOW_RUN_ID')
 model_uri = mlflow.get_run(run_id).info.artifact_uri + '/balanced_rf_model'
-mlflow.artifacts.download_artifacts(model_uri, dst_path='./monitoring/model')
+mlflow.artifacts.download_artifacts(model_uri, dst_path='./model')
