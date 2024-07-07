c04a1e30b49e574f73abc3f89c4a87631c9ce108
divakaivan <timee98@outlook.com>
2024-07-07 22:23:10 +0900

    [orchestration] add batch model predict flow

---
+++ prefect_orchestration/batch_model_predict.py
@@ -0,0 +1,71 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+from prefect import flow, task
+from prefect.flow_runs import pause_flow_run
+from make_monitoring_ui_artifacts import load_model_from_mlflow
+
+@task
+def read_new_data(table_name: str) -> pd.DataFrame:
+    """Reads data to be predicted from the database"""
+    
+    db_params = {
+        'host': os.getenv('db_host'),
+        'port': os.getenv('db_port'), 
+        'database': os.getenv('db_name'),
+        'user': os.getenv('db_username'),
+        'password': os.getenv('db_password')
+    }
+    engine = create_engine(f'postgresql+psycopg2://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}:{db_params["port"]}/{db_params["database"]}')
+    query = f"SELECT * FROM {table_name}"
+    data = pd.read_sql_query(query, engine)
+    engine.dispose()
+
+    return data
+
+@task
+def predict(model: object, data: pd.DataFrame, get_probs: bool = False) -> pd.DataFrame:
+    """
+    Predicts on the data using the model provided. 
+    If get_probs is True, it also returns the probabilities of the predictions.
+    """
+    
+    y_pred = model.predict(data)
+
+    data_with_predictions = data.copy()
+    data_with_predictions['FraudFound_P'] = y_pred
+
+    if get_probs:
+        y_probs = model.predict_proba(data)[:, 1]
+        data_with_predictions['FraudFound_P_Prob'] = y_probs
+
+    return data_with_predictions
+
+@task
+def save_predictions(data: pd.DataFrame, table_name: str) -> None:
+    """Saves the predictions to the database"""
+
+    db_params = {
+        'host': os.getenv('db_host'),
+        'port': os.getenv('db_port'), 
+        'database': os.getenv('db_name'),
+        'user': os.getenv('db_username'),
+        'password': os.getenv('db_password')
+    }
+    engine = create_engine(f'postgresql+psycopg2://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}:{db_params["port"]}/{db_params["database"]}')
+    data.to_sql(table_name, engine, if_exists='append', index=False)
+    engine.dispose()
+
+    return None
+
+@flow(log_prints=True)
+def batch_model_predict():
+
+    table_name = pause_flow_run(wait_for_input=str)
+    data = read_new_data(table_name)
+    model = load_model_from_mlflow()
+    data_with_predictions = predict(model, data)
+    save_predictions(data_with_predictions, 'model_data_w_predictions')
+
+if __name__ == '__main__':
+    batch_model_predict()
--- prefect_orchestration/serve_flows.py
+++ prefect_orchestration/serve_flows.py
@@ -1,7 +1,12 @@
+"""
+This script is used to serve the flows as deployments to Prefect Cloud
+"""
+
 from prefect import serve
-from insurance_fraud_model import insurance_fraud_model
+from train_insurance_fraud_model import insurance_fraud_model
 from raw_kaggle_to_gcs import raw_kaggle_to_gcs
 from make_monitoring_ui_artifacts import make_monitoring_ui_artifacts
+from batch_model_predict import batch_model_predict
 
 if __name__ == "__main__":
     raw_kaggle_to_gcs_deply = raw_kaggle_to_gcs.to_deployment(
@@ -16,9 +21,14 @@ if __name__ == "__main__":
         name='Create Monitoring UI Artifacts',
         tags=['monitoring', 'model', 'data', 'quality']
     )
+    batch_model_predict_deploy = batch_model_predict.to_deployment(
+        name='Batch Model Predict',
+        tags=['model', 'predict', 'batch']
+    )
 
     serve(
         raw_kaggle_to_gcs_deply,
         model_pipe_flow_deploy,
         make_monitoring_ui_artifacts_deploy,
+        batch_model_predict_deploy
     )
